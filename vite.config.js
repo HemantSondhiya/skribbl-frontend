bfe8b5bb75fd2e3ce1671ab088258c6e56492c74
200~anonymoussamachar-prog <anonymoussamachar@gmail.com>
2026-03-22 02:29:53 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,25 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+const target = 'http://skribbl-env.eba-cuuiauxr.eu-north-1.elasticbeanstalk.com';
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   define: {
     global: 'window',
   },
+  server: {
+    proxy: {
+      '/api': {
+        target,
+        changeOrigin: true,
+      },
+      '/ws': {
+        target,
+        changeOrigin: true,
+        ws: true,
+      }
+    }
+  }
 })
