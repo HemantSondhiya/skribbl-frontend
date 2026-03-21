5f88d6419b3d89064acc9359c1b9aee6d5a96066
200~anonymoussamachar-prog <anonymoussamachar@gmail.com>
2026-03-22 02:24:44 +0530

    use https url now

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,11 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-  const target = env.VITE_API_BASE_URL || 'http://localhost:8080';
-
-  return {
-    plugins: [react(), tailwindcss()],
-    define: {
-      global: 'window',
-    },
-    server: {
-      proxy: {
-        '/api': {
-          target,
-          changeOrigin: true,
-        },
-        '/ws': {
-          target,
-          changeOrigin: true,
-          ws: true,
-        }
-      }
-    }
-  }
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  define: {
+    global: 'window',
+  },
 })
